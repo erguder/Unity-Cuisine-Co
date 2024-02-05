813d5ddf23b4aee6039aa7f61d8dee039208f9e2
Selahaddin <erguder.selahaddin@gmail.com>
2024-02-05 03:09:47 +0300

    routes are setted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioOption2Component } from './portfolio-option2/portfolio-option2.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HeroComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'portfolio', component: PortfolioOption2Component},
+  { path: 'faq', component: FaqComponent},
+  { path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
