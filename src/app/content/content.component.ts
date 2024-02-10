61bd9f385b472129e465fb8e540429e863394976
Selahaddin <erguder.selahaddin@gmail.com>
2024-02-10 20:59:07 +0300

    featured done

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -9,13 +9,30 @@ import { DataService } from '../services/data.service';
 export class ContentComponent implements OnInit {
 
   foodData: { name: string; image: string; ingredients: string; recipe: string; origin: string; category: string; }[];
+  randomFoodItems: { name: string; image: string; ingredients: string; recipe: string; origin: string; category: string; }[];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
-
     this.dataService.getFoodData().subscribe(data => {
       this.foodData = data.foodArray;
+      this.getRandomFoodItems();
     });
+  }
+
+  getRandomFoodItems(): void {
+    const randomIndexes = this.getRandomIndexes(this.foodData.length, 4);
+    this.randomFoodItems = randomIndexes.map(index => this.foodData[index]);
+  }
+
+  getRandomIndexes(max: number, count: number): number[] {
+    const indexes: number[] = [];
+    while (indexes.length < count) {
+      const randomIndex = Math.floor(Math.random() * max);
+      if (!indexes.includes(randomIndex)) {
+        indexes.push(randomIndex);
+      }
+    }
+    return indexes;
   }
 }
